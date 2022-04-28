a3f39d516d7a6b22dfeddcff4f1f6af86922f59e
kjdv91 <kjdvdevelop@gmail.com>
2022-04-28 14:46:14 -0500

    primera revisión

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { ethers } from 'ethers';
+import { useState } from 'react'
+import Words from './contract/Words.json'; 
+
+const contractAddress = '0x4B72645ab6187b31f7Cc5F96aCf169Eefc1dCE14';
+
+function App () {
+
+  const [words, setWords] = useState([]);
+  const [word, setWord] = useState();
+  const [Loading, setLoading] = useState(false);
+
+
+  async function fetch (){
+  if(typeof window.ethereum !== 'undefined'){
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const contract = new ethers.Contract(contractAddress, Words.abi, provider);
+
+    try{
+      const data = await contract.getWord();
+      setWords(data);
+    }catch(err){
+      console.log("Error:", err);
+    }
+    
+    
+  };
+  
+
+}; 
+
+//connect wallet 
+async function requestAccount(){
+  await window.ethereum.request({method: "eth_requestAccounts"});
+}
+
+async function  newWord(){
+  if(!word) return;
+  if(typeof window.ethereum !== 'undefined'){
+    await requestAccount();
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, Words.abi, signer);
+    setLoading(true);
+    const transaction = await contract.setWord(word);
+    await transaction.wait();
+
+    fetch();
+    setLoading(false);
+
+    
+
+    
+
+  }
+
+
+};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+      <button onClick={fetch}>Mostrar Palabras</button>
+      <hr></hr>
+      
+      
+      <br></br>
+      <input placeholder='Nueva palabra' onChange={e => setWord(e.target.value)}/>
+      <button onClick={newWord}>Crear Palabra</button>
+      <br/> <br/>
+      {Loading && <p>Cargando..</p>} {/*renderizado condicional*/}
+      <ul className="Words">
+        {words.map((word) => (
+          <li key={word}>{word}</li>
+        ))}
+      </ul> 
+
+      
     </div>
   );
 }
